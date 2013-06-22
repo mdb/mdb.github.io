217aac84f5b69e69f326f0ff0efe63b2a0d06c2f
Mike Ball <mikedball@gmail.com>
2013-06-21 23:02:14 -0400

    Created PostsController

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,9 +3,16 @@ require 'spec_helper'
 describe PostsController do
 
   describe "GET 'index'" do
-    it "returns http success" do
+    before :each do
       get 'index'
+    end
+
+    it "returns http success" do
       response.should be_success
+    end
+
+    it "creates a @posts instance variable with all the posts" do
+      assigns(:posts).should eq Post.all      
     end
   end
 
