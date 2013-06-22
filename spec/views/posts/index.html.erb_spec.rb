f1692978adc9e2c28226c751dba7171d8c2b85f3
Mike Ball <mikedball@gmail.com>
2013-06-21 22:58:46 -0400

    Added posts index view

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe "posts/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    fake_post = Post.create :title => "test title", :content => "test content"
+    @posts = [fake_post, fake_post]
+    render :template => "posts/index", :posts => Post.all
+  end
+
+  it "displays a 'Posts' heading" do
+    rendered.should have_selector('h1', :text => "Posts")
+  end
+
+  it "displays a list of posts, each with a title" do
+    rendered.should have_selector('li h2', :text => "test title")
+  end
+
+  it "displays a list of posts, each with content" do
+    rendered.should have_selector('li p', :text => "test content")
+  end
 end
