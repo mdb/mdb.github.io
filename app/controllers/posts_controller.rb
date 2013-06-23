ae9a8631ef6e1bbaa96d15520ccf1695d4cd4083
Mike Ball <mikedball@gmail.com>
2013-06-23 00:25:25 -0400

    Added create and show controller methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,34 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+
+    respond_to do |format|
+      if @post.save
+        format.html  { redirect_to(@post,
+          :notice => 'Post was successfully created.') }
+        format.json  { render :json => @post,
+          :status => :created, :location => @post }
+      else
+        format.html  { render :action => "new" }
+        format.json  { render :json => @post.errors,
+          :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+   
+    respond_to do |format|
+      format.html  # show.html.erb
+      format.json  { render :json => @post }
+    end
+  end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -16,4 +16,38 @@ describe PostsController do
     end
   end
 
+  describe "GET 'new'" do
+    before :each do
+      get 'new'
+    end
+
+    it "returns http success" do
+      response.should be_success
+    end
+
+    it "creates a new post in a @post instance variable" do
+      assigns(:post).class.should eq Post
+    end
+  end
+
+  describe "POST 'create'" do
+    before :each do
+      post 'create'
+    end
+
+    it "redirects to the post's permalink" do
+      response.should redirect_to('/posts/1')
+    end
+  end
+
+  describe "GET 'show'" do
+    before :each do
+      @post = Post.create(:title => "test title", :content => "")
+      get 'show', :id => 1 
+    end
+
+    it "returns http success" do
+      response.should be_success
+    end
+  end
 end
