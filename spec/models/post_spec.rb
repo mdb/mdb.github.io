4940b5b217a55cd89d7efc53fdb13f1db1143ba7
Mike Ball <mikedball@gmail.com>
2013-06-23 12:12:29 -0400

    Post now accounts for tags

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,11 +9,34 @@ describe Post do
     it "exists as a method on a Post" do
       @post.respond_to?(:title).should eq true
     end
+
+    it "is required" do
+      Post.new(:title => nil).should_not be_valid
+    end
+
+    it "must be 5 characters" do
+      Post.new(:title => 'four').should_not be_valid
+      Post.new(:title => 'at least five chars').should be_valid
+    end
+
+    it "accepts nested tag attributes" do
+      @post.should accept_nested_attributes_for :tags
+    end
   end
 
   describe "#content" do
     it "exists as a method on a Post" do
       @post.respond_to?(:content).should eq true
+    end
+  end
+
+  describe "#tags" do
+    it "exists as a method on a Post" do
+      @post.respond_to?(:content).should eq true
+    end
+
+    it "is an emptry array by default" do
+      @post.tags.should eq [] 
     end
   end
 end
