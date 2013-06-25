900299308b8a1e9e2e422c2ff73eb5001fa748d0
Mike Ball <mikedball@gmail.com>
2013-06-24 23:29:15 -0400

    Created post edit action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,10 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @post = Post.find_by_slug(params[:id])
+  end
+
   def create
     @post = Post.new(params[:post])
 
@@ -16,6 +20,18 @@ class PostsController < ApplicationController
           :notice => 'Post was successfully created.') }
       else
         format.html  { render :action => "new" }
+      end
+    end
+  end
+  
+  def update
+    @post = Post.find_by_slug(params[:id])
+
+    respond_to do |format|
+      if @post.update_attributes(params[:id])
+        format.html { redirect_to(@post, :notice => 'Post was successfully updated.') }
+      else
+        format.html { render :action => "edit" }
       end
     end
   end
