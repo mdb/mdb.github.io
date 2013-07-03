f21230c8014cd3b2c23854b6bc5f129408ea78a0
Mike Ball <mikedball@gmail.com>
2013-07-03 07:43:18 -0400

    Removed white space

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ class Post < ActiveRecord::Base
     :length => { :minimum => 5 }
 
   has_many :tags
-                               
+
   accepts_nested_attributes_for :tags,
     :allow_destroy => :true,
     :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
