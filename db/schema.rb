b5b4f69e67549fb8ecb1a938d8a14038a4dcd50f
Mike Ball <mikedball@gmail.com>
2013-07-04 08:30:13 -0400

    AddThumbnailToPosts migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130703184251) do
+ActiveRecord::Schema.define(:version => 20130704122618) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at",                   :null => false
-    t.datetime "updated_at",                   :null => false
+    t.datetime "created_at",                               :null => false
+    t.datetime "updated_at",                               :null => false
     t.string   "slug"
-    t.boolean  "active",     :default => true
+    t.boolean  "active",                 :default => true
+    t.string   "thumbnail_file_name"
+    t.string   "thumbnail_content_type"
+    t.integer  "thumbnail_file_size"
+    t.datetime "thumbnail_updated_at"
   end
 
   create_table "taggings", :force => true do |t|
