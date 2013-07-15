987eb0b3fff694337d7eba0692226dbf0c073e52
Mike Ball <mikedball@gmail.com>
2013-07-15 07:12:23 -0400

    Adjusted asset configuration
    Following a combo of advice from these 2 sourced:
    http://railscasts.com/episodes/415-upgrading-to-rails-4?view=asciicast
    http://www.andylindeman.com/2013/05/08/my-assets-are-no-longer-compressed-in-rails-4-halp.html

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env)
 
 module Mdb
   class Application < Rails::Application
@@ -46,12 +43,6 @@ module Mdb
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
-
-    # Enable the asset pipeline
-    config.assets.enabled = true
-
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
 
     # Basic HTTP authentication
     config.username = 'username'
