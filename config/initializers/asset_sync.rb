349cd6835c26f927cb394480837191378d2555d1
Mike Ball <mikedball@gmail.com>
2013-09-10 07:41:41 -0400

    Fine-tuning asset_sync config for Heroku
    Attempting to debug deployments.

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,13 +1,13 @@
-# Since this gem is only loaded with the assets group, we have to check to 
+# Since this gem is only loaded with the assets group, we have to check to
 # # see if it's defined before configuring it.
 if defined?(AssetSync)
   AssetSync.configure do |config|
-    config.fog_provider = 'AWS'
+    config.fog_provider = ENV['FOG_DIRECTORY']
     config.aws_access_key_id = ENV['AWS_ACCESS_KEY']
     config.aws_secret_access_key = ENV['AWS_SECRET_KEY']
     config.fog_directory = ENV['FOG_DIRECTORY']
 
-    # Fail silently.  Useful for environments such as Heroku
-    config.fail_silently = true
+    # Fail silently. Useful for environments such as Heroku
+    config.fail_silently = false
   end
 end
