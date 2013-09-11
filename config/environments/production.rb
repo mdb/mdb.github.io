03fc2bbd4a99e2efd150a0f50d337c40c143384e
Mike Ball <mikedball@gmail.com>
2013-09-11 15:18:21 -0400

    Tweaking paperclip configuration
    Repurposing existing AWS env variables.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Mdb::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
+      :bucket => ENV['FOG_DIRECTORY'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
