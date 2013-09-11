7f81cab8ae9c308262c1f035a36561c8992796ff
Mike Ball <mikedball@gmail.com>
2013-09-11 07:28:40 -0400

    Fine-tuning configuration; debugging asset_sync

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Mdb::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://clapclapexcitement.s3-website-us-east-1.amazonaws.com"
+  config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
