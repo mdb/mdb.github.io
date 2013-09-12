1e4e26516e87dca6931d4e8a801f17c7a731f27a
Mike Ball <mikedball@gmail.com>
2013-09-11 22:04:20 -0400

    Removed duplicate test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -18,10 +18,6 @@ describe Post do
       Post.new(:title => 'four').should_not be_valid
       Post.new(:title => 'at least five chars').should be_valid
     end
-
-    it "responds to #tag_list" do
-      @post.respond_to?(:tag_list).should eq true
-    end
   end
 
   describe "#content" do
