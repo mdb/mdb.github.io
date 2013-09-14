49a89f4c2c5948bcb33379346b7e8aab21dd016e
Mike Ball <mikedball@gmail.com>
2013-09-14 14:49:47 -0400

    Added slug to project model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130914180604) do
+ActiveRecord::Schema.define(version: 20130914184829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20130914180604) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "active",      default: true
+    t.string   "slug"
   end
 
   create_table "taggings", force: true do |t|
