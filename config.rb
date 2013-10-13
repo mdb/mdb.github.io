70eb25d904ef144ada391901ddce6cffeb403d1e
Mike Ball <mikedball@gmail.com>
2013-10-13 10:56:37 -0400

    Created projects blog

--- config.rb
+++ config.rb
@@ -1,57 +1,10 @@
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
+# Blog
 activate :blog do |blog|
+  blog.name = "blog"
   blog.prefix = "blog"
   blog.tag_template  = "tag.html"
 end
@@ -60,6 +13,18 @@ page "blog/*", :layout => :article
 page "blog", :layout => :layout
 page "blog/tags/*", :layout => :layout
 
+# Portfolio
+activate :blog do |blog|
+  blog.name = "projects"
+  blog.prefix = "projects"
+  blog.permalink = ":title.html"
+  blog.tag_template  = "tag.html"
+end
+
+page "projects/*", :layout => :project
+page "projects", :layout => :layout
+page "projects/tags/*", :layout => :layout
+
 activate :directory_indexes
 
 # Add bower_components directory to asset pipeline
@@ -67,16 +32,9 @@ sprockets.append_path File.join "#{root}", "bower_components"
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
   activate :minify_css
-
-  # Minify Javascript on build
   activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
+  activate :asset_hash
   activate :relative_assets
 
   # Or use a different image path
---
+++ source/projects/2012-10-18-my-project.html.markdown
@@ -0,0 +1,7 @@
+---
+title: Test Project
+date: 2012/10/18
+tags: foo, bar
+---
+
+A Project
