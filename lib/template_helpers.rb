277344623031b44d7262f8c910feb551c2e20379
Mike Ball <mikedball@gmail.com>
2013-10-29 22:29:12 -0400

    Adds last_class template helper

--- lib/template_helpers.rb
+++ lib/template_helpers.rb
@@ -1,4 +1,12 @@
 module TemplateHelpers
+  def last_class array, index
+    if index + 1 == array.length
+      "last"
+    else
+      ""
+    end
+  end
+
   def blog_thumb_path file_name
     image_path("blog/thumbnails/#{file_name}")
   end
