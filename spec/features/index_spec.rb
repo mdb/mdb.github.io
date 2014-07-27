39fb186ad5679bbe3181ca51f09bd71c09690f0a
Mike Ball <mikedball@gmail.com>
2014-07-27 07:55:41 -0400

    added homepage project gallery tests

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -13,5 +13,27 @@ describe 'index', type: :feature do
     it 'displays the proper section heading' do
       expect(page).to have_selector('h1.divider', text: 'Recent Projects | View all')
     end
+
+    it 'displays the proper gallery markup' do
+      expect(page).to have_selector('ul.gallery')
+    end
+
+    context 'each gallery item' do
+      it 'displays the proper thumbnail link' do
+        expect(page).to have_selector('ul.gallery li.item a[href="/projects/times-grapher/"] img')
+      end
+
+      it 'reports project details label' do
+        expect(page).to have_selector('ul.gallery li.item div.details small', text: 'Project')
+      end
+
+      it 'reports project details heading' do
+        expect(page).to have_selector('ul.gallery li.item div.details h2 a[href="/projects/times-grapher/"]', text: 'TimesGrapher')
+      end
+
+      it 'reports project details tags' do
+        expect(page).to have_selector('ul.gallery li.item div.details ul.tags li.first a[href="/projects/tags/rails"]', text: 'rails')
+      end
+    end
   end
 end
