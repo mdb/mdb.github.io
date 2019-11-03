493e9be0e3318f0b51d6ef0a09f8e36d9d4401b9
Mike Ball <mikedball@gmail.com>
2019-11-03 14:07:44 -0500

    remove middleman source code

--- source/javascripts/all.js
+++
@@ -1,8 +0,0 @@
-//= require jquery/jquery
-//= require underscore/underscore
-//= require_tree
-
-_.templateSettings = {
-  evaluate : /\{\[([\s\S]+?)\]\}/g,
-  interpolate : /\{\{([\s\S]+?)\}\}/g
-};
