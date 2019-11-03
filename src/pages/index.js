dc07697c86fe2302902ce89a36fc283dfb7e15ec
Mike Ball <mikedball@gmail.com>
2019-11-03 15:49:51 -0500

    filter unpublished blog posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,7 +48,10 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(
+      filter: { frontmatter: { published: { ne: false } } },
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
       edges {
         node {
           excerpt
