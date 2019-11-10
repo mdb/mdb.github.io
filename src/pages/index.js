6873d11fea8d20d2ec53c9c26b3b6c7a63cb6cff
Mike Ball <mikedball@gmail.com>
2019-11-10 14:08:58 -0500

    use ProjectList component on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout'
 import BlogPostList from '../components/blog-post-list'
-import ProjectList from '../components/blog-post-list'
+import ProjectList from '../components/project-list'
 
 class Index extends React.Component {
   render() {
