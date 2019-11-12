c4d6e522706026943115fc8bfa7f3ca28c72a5d8
Mike Ball <mikedball@gmail.com>
2019-11-12 08:07:10 -0500

    create tag list styles

--- src/components/tag-list.js
+++ src/components/tag-list.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import tagStyles from './tag-list.module.css'
 
 class TagList extends React.Component {
   render() {
     return (
-      <ul>
+      <ul className={tagStyles.tags}>
         {this.props.tags.map(tag => {
           return(
-            <li key={tag}>
+            <li className={tagStyles.tag} key={tag}>
               <Link to={`/tags/${tag}`}>
                 {tag}
               </Link>
