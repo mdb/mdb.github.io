309b09d200f01c707ae078274afb13a9be0b259d
Mike Ball <mikedball@gmail.com>
2019-11-30 09:43:28 -0500

    simplify layout & use css module

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,36 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
-import { rhythm, scale } from '../utils/typography'
+import layoutStyles from './layout.module.css'
 
 class Layout extends React.Component {
   render() {
     const { title, children } = this.props
 
     return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
+      <div className={layoutStyles.layout}>
         <header>
-          <h1
-            style={{
-              ...scale(1.5),
-              marginBottom: rhythm(1.5),
-              marginTop: 0,
-            }}
-          >
-            <Link
-              style={{
-                textDecoration: `none`,
-                color: `inherit`,
-              }}
-              to={`/`}
-            >
+          <h1>
+            <Link to={`/`}>
               {title}
             </Link>
           </h1>
