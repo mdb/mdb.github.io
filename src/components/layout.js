10906920f3adc89de7b5c8957419e8ce53f10296
Mike Ball <mikedball@gmail.com>
2019-12-01 16:29:26 -0500

    add basic layout styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,19 +4,24 @@ import layoutStyles from './layout.module.css'
 
 class Layout extends React.Component {
   render() {
-    const { title, children } = this.props
+    const { children } = this.props
 
     return (
-      <div className={layoutStyles.layout}>
-        <header>
-          <h1>
+      <div>
+        <header className={layoutStyles.header}>
+          <h1 className={layoutStyles.mark}>
             <Link to={`/`}>
-              {title}
+              Mike Ball
             </Link>
           </h1>
         </header>
-        <main>{children}</main>
-        <footer>
+        <main className={layoutStyles.main}>{children}</main>
+        <footer className={layoutStyles.footer}>
+          <section>
+            <h2>Mike Ball</h2>
+            <p>I live in Philadelphia and work as a multi-disciplinary software developer and designer.</p>
+            <p>I am especially excited to work with journalists, artists, and nonprofits. I like to work in JavaScript, Ruby, Golang, new technologies, and on open source projects. Interested in working with me? Get in touch.</p>
+          </section>
         </footer>
       </div>
     )
