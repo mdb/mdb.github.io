053f72137a50c8ab3581602b04595fc177a5a835
Mike Ball <mikedball@gmail.com>
2019-12-01 07:20:27 -0500

    simplify blog post templates

--- src/components/blog-post-list.js
+++ src/components/blog-post-list.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { rhythm } from '../utils/typography'
 import TagList from './tag-list'
 import Thumbnail from './thumbnail'
 
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,7 +3,6 @@ import { Link, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import Layout from '../components/layout'
 import TagList from '../components/tag-list'
-import { rhythm, scale } from '../utils/typography'
 
 const BlogPost = ({ pageContext, data }) => {
   const post = data.markdownRemark
@@ -14,55 +13,26 @@ const BlogPost = ({ pageContext, data }) => {
     <Layout title={siteTitle}>
       <article>
         <header>
-          <h1
-            style={{
-              marginTop: rhythm(1),
-              marginBottom: 0,
-            }}
-          >
-            {post.frontmatter.title}
-          </h1>
+          <h1>{post.frontmatter.title}</h1>
           <Img fluid={post.frontmatter.thumbnail ? post.frontmatter.thumbnail.childImageSharp.fluid : ''} />
-          <p
-            style={{
-              ...scale(-1 / 5),
-              display: `block`,
-              marginBottom: rhythm(1),
-            }}
-          >
-            {post.frontmatter.date}
-          </p>
+          <p>{post.frontmatter.date}</p>
           <TagList tags={post.frontmatter.tags} />
         </header>
         <section dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
       </article>
-
       <nav>
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
+        <ul>
           <li>
             {previous && (
               <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
+                &laquo; {previous.frontmatter.title}
               </Link>
             )}
           </li>
           <li>
             {next && (
               <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
+                {next.frontmatter.title} &raquo;
               </Link>
             )}
           </li>
