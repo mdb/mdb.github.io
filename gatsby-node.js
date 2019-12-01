b65b7caf188ce188e2a186bea4ba4cfa35aec4dc
Mike Ball <mikedball@gmail.com>
2019-12-01 15:11:02 -0500

    use simple strings over template strings

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,5 @@
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require('path')
+const { createFilePath } = require('gatsby-source-filesystem')
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
