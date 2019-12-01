500abe236e4b62423593a2bb618b298ca4e4061d
Mike Ball <mikedball@gmail.com>
2019-12-01 16:48:38 -0500

    simplify thumb styles

--- src/components/external-thumbnail.js
+++ src/components/external-thumbnail.js
@@ -3,7 +3,7 @@ import React from 'react'
 class ExternalThumbnail extends React.Component {
   render() {
     return (
-      <div style={{ maxWidth: 200 }}>
+      <div>
         <a href={this.props.link}>
           <img alt={this.props.alt} src={this.props.imageUrl} />
         </a>
