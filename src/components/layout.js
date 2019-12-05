fe675ee6b04e79ecd452e57a9d3962579b820fa1
Mike Ball <mikedball@gmail.com>
2019-12-05 06:28:54 -0500

    add footer styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import layoutStyles from './layout.module.css'
+import styles from './layout.module.css'
+import Instagram from '../components/instagram'
 
 class Layout extends React.Component {
   render() {
@@ -8,19 +9,51 @@ class Layout extends React.Component {
 
     return (
       <div>
-        <header className={layoutStyles.header}>
-          <h1 className={layoutStyles.mark}>
+        <header className={styles.header}>
+          <h1 className={styles.mark}>
             <Link to={`/`}>
               {title}
             </Link>
           </h1>
         </header>
-        <main className={layoutStyles.main}>{children}</main>
-        <footer className={layoutStyles.footer}>
+        <main className={styles.main}>{children}</main>
+        <footer className={styles.footer}>
           <section>
-            <h2>{title}</h2>
-            <p>I live in Philadelphia and work as a multi-disciplinary software developer and designer.</p>
-            <p>I am especially excited to work with journalists, artists, and nonprofits. I like to work in JavaScript, Ruby, Golang, new technologies, and on open source projects. Interested in working with me? Get in touch.</p>
+            <div className={styles.quarterColumn}>
+              <h2>{title}</h2>
+              <p>I live in Philadelphia and work as a multi-disciplinary software developer and graphic artist.</p>
+              <p>I am especially excited to work with artists, journalists, and organizations with big ideas. I like to work on collaborative teams leveraging cloud native technologies, and on open source projects. Interested in working with me? Get in touch.</p>
+            </div>
+            <div className={styles.halfColumn}>
+              <h2>Instagram</h2>
+              <Instagram />
+            </div>
+            <div className={styles.quarterColumn}>
+              <h2>Etc.</h2>
+              <ul>
+                <li>
+                  <a href='TODO'>Resume</a>
+                </li>
+                <li>
+                  <a href='http://github.com/mdb'>GitHub</a>
+                </li>
+                <li>
+                  <a href='https://instagram.com/clapclapexcitement'>Instagram</a>
+                </li>
+                <li>
+                  <a href='http://twitter.com/clapexcitement'>Twitter</a>
+                </li>
+                <li>
+                  <a href='TODO'>LinkedIn</a>
+                </li>
+                <li>
+                  <a href='TODO'>RSS Feed</a>
+                </li>
+                <li>
+                  <a href='TODO'>Atom Feed</a>
+                </li>
+              </ul>
+            </div>
           </section>
         </footer>
       </div>
