8387410bf1305687f3f4233441ca1c29635977ad
Mike Ball <mikedball@gmail.com>
2020-10-26 09:01:55 -0400

    add store to footer & tweak copy a bit

--- themes/mdb/assets/js/main.js
+++ themes/mdb/assets/js/main.js
@@ -24,26 +24,36 @@ document.addEventListener('DOMContentLoaded', () => {
     return;
   }
 
+  function populateShopFeed(data) {
+    let footer = document.querySelector('footer ul.store-feed')
+        gallery = document.querySelector('ul.gallery.store-feed'),
+        items = data.slice(0, 4).map(item => {
+          let url = `https://tiendah.bigcartel.com/${item.url}`;
+
+          return `
+            <li class="item">
+              <a class="thumbnail" href="${url}">
+                <img src="${item.images[0].secure_url}" />
+              </a>
+              <div class="details">
+                <aside>$${item.price}</aside>
+                <h2><a href="${url}">${item.name}</a></h2>
+                <p>${item.description}</p>
+                <p><a href="${url}">Buy print</a></p>
+              </div>
+            </li>`;
+        });
+
+      footer.innerHTML = items.join('');
+
+      if (gallery) {
+        gallery.innerHTML = items.join('');
+      }
+  }
+
   fetch('https://api.bigcartel.com/tiendah/products.json')
     .then(response => response.json())
     .then(data => {
-      items = data.slice(0, 8).map(item => {
-        let url = `https://tiendah.bigcartel.com/${item.url}`;
-
-        return `
-          <li class="item">
-            <a class="thumbnail" href="${url}">
-              <img src="${item.images[0].secure_url}" />
-            </a>
-            <div class="details">
-              <aside>$${item.price}</aside>
-              <h2><a href="${url}">${item.name}</a></h2>
-              <p>${item.description}</p>
-              <p><a href="${url}">Buy print</a></p>
-            </div>
-          </li>`;
-      });
-
-      gallery.innerHTML = items.join('');
+      populateShopFeed(data);
     });
 });
