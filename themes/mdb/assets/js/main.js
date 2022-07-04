5c37f2120c67861126fc939e7630a70d16be8723
Mike Ball <mikedball@gmail.com>
2022-07-04 06:05:57 -0400

    use const instead of let

--- themes/mdb/assets/js/main.js
+++ themes/mdb/assets/js/main.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   function populateIGFeeds(data) {
-    let footer = document.querySelector('footer ul.ig-feed'),
-        gallery = document.querySelector('ul.gallery.ig-feed'),
-        images = data.slice(0, 8).map(image => {
-          return `<li class="item"><a class="thumbnail" href="${image.permalink}"><img src="${image.media_url}" /></a></li>`;
-        });
+    const footer = document.querySelector('footer ul.ig-feed'),
+      gallery = document.querySelector('ul.gallery.ig-feed'),
+      images = data.slice(0, 8).map(image => {
+        return `<li class="item"><a class="thumbnail" href="${image.permalink}"><img src="${image.media_url}" /></a></li>`;
+      });
 
     footer.innerHTML = images.join('');
 
@@ -19,30 +19,30 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(response => response.json())
     .then(populateIGFeeds);
 
-  let gallery = document.querySelector('ul.gallery.store-feed');
+  const gallery = document.querySelector('ul.gallery.store-feed');
   if (!gallery) {
     return;
   }
 
   function populateShopFeeds(data) {
-    let footer = document.querySelector('footer ul.store-feed')
-        gallery = document.querySelector('ul.gallery.store-feed'),
-        items = data.slice(0, 4).map(item => {
-          let url = `https://tiendah.bigcartel.com/${item.url}`;
+    const footer = document.querySelector('footer ul.store-feed'),
+      gallery = document.querySelector('ul.gallery.store-feed'),
+      items = data.slice(0, 4).map(item => {
+        const url = `https://tiendah.bigcartel.com/${item.url}`;
 
-          return `
-            <li class="item">
-              <a class="thumbnail" href="${url}">
-                <img src="${item.images[0].secure_url}" />
-              </a>
-              <div class="details">
-                <aside>$${item.price}</aside>
-                <h2><a href="${url}">${item.name}</a></h2>
-                <p>${item.description}</p>
-                <p><a href="${url}">Buy print</a></p>
-              </div>
-            </li>`;
-        });
+        return `
+          <li class="item">
+            <a class="thumbnail" href="${url}">
+              <img src="${item.images[0].secure_url}" />
+            </a>
+            <div class="details">
+              <aside>$${item.price}</aside>
+              <h2><a href="${url}">${item.name}</a></h2>
+              <p>${item.description}</p>
+              <p><a href="${url}">Buy print</a></p>
+            </div>
+          </li>`;
+      });
 
       footer.innerHTML = items.join('');
 
@@ -55,8 +55,8 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(response => response.json())
     .then(populateShopFeeds);
 
-  let button = document.getElementsByTagName('button')[0],
-      header = button.parentElement;
+  const button = document.getElementsByTagName('button')[0],
+    header = button.parentElement;
 
   button.addEventListener('click', () => {
     if (header.className === '') {
