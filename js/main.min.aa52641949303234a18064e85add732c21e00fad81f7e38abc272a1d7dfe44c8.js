1b35619edfdd0e2c0a68e5cde9d299f57c3866be
mdb <mikedball@gmail.com>
2022-07-04 10:31:52 +0000

    Deploying to gh-pages from @ mdb/mdb.github.io@f1b5f9d9645cdc325931bca4c5af6ddc9b4e9f85 🚀

--- js/main.min.aa52641949303234a18064e85add732c21e00fad81f7e38abc272a1d7dfe44c8.js
+++ js/main.min.aa52641949303234a18064e85add732c21e00fad81f7e38abc272a1d7dfe44c8.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded",()=>{function n(e){const t=document
             <a class="thumbnail" href="${e.permalink}">
               <img src="${e.media_url}" />
             </a>
-          </li>`).join('');t.forEach(e=>e.innerHTML=n)}fetch("https://mikeball.info/ig-feed/ig/media.json").then(e=>e.json()).then(n);function s(e){const t=document.querySelectorAll("ul.store-feed"),n=e.slice(0,4).map(e=>{const t=`https://tiendah.bigcartel.com/${e.url}`;return`
+          </li>`).join("");t.forEach(e=>e.innerHTML=n)}fetch("https://mikeball.info/ig-feed/ig/media.json").then(e=>e.json()).then(n);function s(e){const t=document.querySelectorAll("ul.store-feed"),n=e.slice(0,4).map(e=>{const t=`https://tiendah.bigcartel.com/${e.url}`;return`
           <li class="item">
             <a class="thumbnail" href="${t}">
               <img src="${e.images[0].secure_url}" />
@@ -14,4 +14,4 @@ document.addEventListener("DOMContentLoaded",()=>{function n(e){const t=document
               <p>${e.description}</p>
               <p><a href="${t}">Buy print</a></p>
             </div>
-          </li>`}).join('');t.forEach(e=>e.innerHTML=n)}fetch("https://api.bigcartel.com/tiendah/products.json").then(e=>e.json()).then(s);const t=document.getElementsByTagName("button")[0],e=t.parentElement;t.addEventListener("click",()=>{if(e.className===''){e.className="toggled";return}e.className=''})})
+          </li>`}).join("");t.forEach(e=>e.innerHTML=n)}fetch("https://api.bigcartel.com/tiendah/products.json").then(e=>e.json()).then(s);const t=document.getElementsByTagName("button")[0],e=t.parentElement;t.addEventListener("click",()=>{if(e.className===""){e.className="toggled";return}e.className=""})})
