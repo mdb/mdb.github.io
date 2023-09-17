4ec968aa89743b2df6a3979973ba30732fae5868
mdb <mikedball@gmail.com>
2023-09-17 11:12:02 +0000

    Deploying to gh-pages from @ mdb/mdb.github.io@c98650b376619aca00e5b007690440285a260de2 🚀

--- js/main.min.5d3e1e570343926cb3ff35c8e4730eac1200594ead7be05a1c97996e632be67e.js
+++ js/main.min.5d3e1e570343926cb3ff35c8e4730eac1200594ead7be05a1c97996e632be67e.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded",()=>{function t(e){e.style.transition="opacity 2s",e.style.opacity="1"}function s(){const e=document.getElementsByTagName("img");for(let n of e)n.complete&&t(n),n.addEventListener("load",()=>{t(n)})}function r(e){const t=document.querySelectorAll("ul.ig-feed"),n=e.slice(0,12).map(e=>`
           <li class="item">
             <a class="thumbnail" href="${e.permalink}">
-              <img src="${e.media_url}" />
+              <img src="${e.github_media_url}" />
             </a>
-          </li>`).join("");t.forEach(e=>e.innerHTML=n)}function c(){return fetch("https://mikeball.info/ig-feed/feeds/instagram-media.json").then(e=>e.json()).then(r)}function o(e){const t=document.querySelectorAll("ul.store-feed"),n=e.slice(0,4).map(e=>{const t=`https://tiendah.bigcartel.com/${e.url}`;return`
+          </li>`).join("");t.forEach(e=>e.innerHTML=n)}function c(){return fetch("https://mikeball.info/feeder/feeds/instagram-media.json").then(e=>e.json()).then(r)}function o(e){const t=document.querySelectorAll("ul.store-feed"),n=e.slice(0,4).map(e=>{const t=`https://tiendah.bigcartel.com/${e.url}`;return`
           <li class="item">
             <a class="thumbnail" href="${t}">
               <img src="${e.images[0].secure_url}" />
@@ -19,4 +19,4 @@ document.addEventListener("DOMContentLoaded",()=>{function t(e){e.style.transiti
             <a href="${e.url}">
               ${e.repo}
             </a>
-          </li>`).join("");t.forEach(e=>e.innerHTML=n)}function l(){return fetch("https://mikeball.info/ig-feed/feeds/github-contributions.json").then(e=>e.json()).then(a)}Promise.all([c(),i()]).then(s);const n=document.getElementsByTagName("button")[0],e=n.parentElement;n.addEventListener("click",()=>{if(e.className===""){e.className="toggled";return}e.className=""})})
+          </li>`).join("");t.forEach(e=>e.innerHTML=n)}function l(){return fetch("https://mikeball.info/feeder/feeds/github-contributions.json").then(e=>e.json()).then(a)}Promise.all([c(),i()]).then(s);const n=document.getElementsByTagName("button")[0],e=n.parentElement;n.addEventListener("click",()=>{if(e.className===""){e.className="toggled";return}e.className=""})})
